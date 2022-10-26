ad2c853dba88ac329f9a79f9c0defef1a198bfab
Greg <greg_sr@hotmail.com>
2022-10-26 19:43:45 +0200

    add gallery component

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,9 +1,18 @@
+import arrowLeft from '../../assets/arrow_left.png';
+import arrowRight from '../../assets/arrow_right.png';
+import { useState } from 'react'
 import './gallery.css';
  
 function Gallery({img}) {
+    const [currentImg, setCurrentImg] = useState(0)
+    const totalImg = img.length;
     return (
         <div className='gallery' style={{
-            backgroundImage: ` url("${img[0]}") ` }}></div>
+            backgroundImage: ` url("${img[currentImg]}") ` }}>
+                { img.length > 1 ? <img className='arrow_side' src={arrowLeft} alt="Left" onClick={() => (currentImg === 0) ? setCurrentImg(img.length-1) : setCurrentImg(currentImg - 1)}/> : null}
+                <span className='gallery_counter'>{currentImg+1}/{totalImg}</span>
+                { img.length > 1 ? <img className='arrow_side' src={arrowRight} alt="Right"  onClick={() => (currentImg === (img.length-1)) ? setCurrentImg(0) : setCurrentImg(currentImg + 1)}/> : null}
+            </div>
     )
 }
 
--- src/pages/lodging/index.js
+++ src/pages/lodging/index.js
@@ -2,12 +2,15 @@ import { useParams } from 'react-router-dom'
 import LocationList from '../../datas/datas.json';
 import Dropdown from '../../components/dropdown';
 import Gallery from '../../components/gallery';
+import ratingOrange from '../../assets/rating_orange.png';
+import ratingGrey from '../../assets/rating_grey.png';
 import './lodging.css';
 
 function Lodging() {
     const { idLodging } = useParams()
     const lodgingFilterByID = LocationList.filter(lodging => lodging.id === idLodging);
     const lodgingDatas = lodgingFilterByID[0];
+    const range = [1, 2, 3, 4, 5]
     // const lodgingEquipements = lodgingDatas.equipments.map((plant) => (<span>{plant}</span>));
 
     return (
@@ -21,6 +24,15 @@ function Lodging() {
                 <div className='host'>
                     <span  className='host-name'>{ lodgingDatas.host.name }</span>
                     <img src={lodgingDatas.host.picture} alt={ lodgingDatas.host.name }  className='host_picture'/>
+                </div>
+            </div>
+            <div  className='info_tag-rating'>
+                <div className='info_tag'>
+                    {lodgingDatas.tags.map((tag) => <span key={tag}  className='tag'>{tag}</span> )}
+            </div>
+                <div>
+                    {range.map((rangeElem) =>
+                lodgingDatas.rating >= rangeElem ? <img src={ratingOrange} alt={lodgingDatas.rating}  key={rangeElem.toString()}/> : <img src={ratingGrey} alt={lodgingDatas.rating}  key={rangeElem.toString()}/>)}
                 </div>
             </div>
             <div className='info_dropdowns'>
